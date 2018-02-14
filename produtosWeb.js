2ac3971cc9034cccec6c4ca9116ae4115bd8bb55
tali_ta <talita.ssoares@hotmail.com>
2018-02-14 01:30:58 +0000

    node snippets

---
+++ produtosWeb.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+var server = http.createServer(function(req,res){
+    res.end("<html><body><h1>Hello World MEAN</h1></body></html>");
+});
+
+server.listen(3000);
+
+
+console.log("servidor rodando")
